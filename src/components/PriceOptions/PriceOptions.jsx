210a0c25c846335355694e1bbd023357f3e9bf48
nahid <nahidrabbi045@gmail.com>
2024-03-26 11:15:11 +0600

    added price options

---
+++ src/components/PriceOptions/PriceOptions.jsx
@@ -0,0 +1,53 @@
+const PriceOptions = () => {
+
+    const priceOptions = [
+        {
+            "id": 1,
+            "name": "Basic Membership",
+            "features": [
+                "Access to gym facilities",
+                "Standard workout equipment",
+                "Limited gym hours",
+                "Access to locker room"
+            ],
+            "price": 30.00
+        },
+        {
+            "id": 2,
+            "name": "Premium Membership",
+            "features": [
+                "Access to gym facilities",
+                "Full range of workout equipment",
+                "Extended gym hours",
+                "Group fitness classes",
+                "Access to locker room"
+            ],
+            "price": 50.00
+        },
+        {
+            "id": 3,
+            "name": "Platinum Membership",
+            "features": [
+                "Access to gym facilities",
+                "Full range of workout equipment",
+                "24/7 gym access",
+                "Personal training sessions",
+                "Access to sauna and spa",
+                "Access to locker room",
+                "Towel service"
+            ],
+            "price": 80.00
+        }
+    ];
+
+    return (
+        <div>
+             <h2 className="text-5xl">Best prices in the town</h2>
+            {
+                priceOptions.map(option => <PriceOptions key={option.id} option={option}></PriceOptions>)
+            }
+        </div>
+    );
+};
+
+export default PriceOptions;
